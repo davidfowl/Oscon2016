1bc682dcd608f74b5a4b198f63a60d255b209ad3
David Fowler <davidfowl@gmail.com>
2016-05-15 17:59:26 -0500

    Added typescript

---
+++ AttendeeList/wwwroot/app.ts
@@ -0,0 +1,34 @@
+interface Attendee {
+    FirstName: string,
+    LastName: string,
+    Company: string,
+    Email: string
+}
+
+function getAttendees(): Promise<Attendee[]> {
+    return new Promise(resolve => {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', '/api/attendees', true)
+        xhr.send(null);
+
+        xhr.onreadystatechange = function (e) {
+            if (xhr.readyState === 4) {
+                var obj = JSON.parse(xhr.responseText) as Attendee[];
+                resolve(obj);
+            }
+        };
+    });
+}
+
+async function init() {
+    var attendees = await getAttendees();
+    var attendeesEl = document.getElementById('attendees');
+
+    for (var i = 0; i < attendees.length; ++i) {
+        var li = document.createElement('li');
+        li.innerText = attendees[i].FirstName + ' ' + attendees[i].LastName + ' (' + attendees[i].Company + ')';
+        attendeesEl.appendChild(li);
+    }
+}
+
+init();
