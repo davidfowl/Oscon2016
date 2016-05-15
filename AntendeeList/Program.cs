a206328de9258b730d6e58c11b880f0d34812e09
David Fowler <davidfowl@gmail.com>
2016-05-15 14:11:53 -0500

    Basic http server

---
+++ AntendeeList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                            .UseKestrel()
+                            .Configure(app =>
+                            {
+                                app.Run(context =>
+                                {
+                                    return context.Response.WriteAsync("Hello World");
+                                });
+                            })
+                            .Build();
+
+            host.Run();
+        }
+    }
+}
